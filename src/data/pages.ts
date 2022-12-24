1b440a457d1b9375c7d517de85cba92598a3770a
ahmadRamadhan-dotcom <ahmadramadhan@mx>
2022-12-24 19:46:22 +0700

    chore: updated

--- src/data/pages.ts
+++ src/data/pages.ts
@@ -1,5 +1,6 @@
 interface Pages {
   id: number;
+  name: string;
   icon: string;
   href: string;
 }
@@ -7,26 +8,31 @@ interface Pages {
 const pages: Pages[] = [
   {
     id: 1,
+    name: "Home",
     icon: "bi-house",
     href: "/",
   },
   {
     id: 2,
+    name: "Projects",
     icon: "bi-file-code",
     href: "/projects",
   },
   {
     id: 3,
+    name: "Links",
     icon: "bi-link-45deg",
     href: "/links",
   },
   {
     id: 4,
+    name: "Snippets",
     icon: "bi-files",
     href: "/snippets",
   },
   {
     id: 5,
+    name: "About",
     icon: "bi-file-earmark-person",
     href: "/about",
   },

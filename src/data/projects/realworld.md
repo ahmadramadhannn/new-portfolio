b351874398e54793448dabffbc045dfbe2022876
ahmadramadhan <ahmadoneperson@gmail.com>
2023-04-15 16:32:06 +0700

    chore: adding one more project

---
+++ src/data/projects/realworld.md
@@ -0,0 +1,14 @@
+---
+project_name: "Realworld app"
+description: "Medium clone called Conduit with react, express, prisma, postgresql"
+slug: "realworld-app"
+image: "/assets/projects/realworld.png"
+singleImage: "/assets/projects/realworld.png"
+githubLink: "https://github.com/ahmadramadhaNn/realworld-app-react-express-postgresql"
+demoLink: "https://realworld-app-react-express-postgresql.vercel.app/"
+tech_stack:
+  - ["React Js", "react"]
+  - ["React Query", "reactquery"]
+  - ["Express JS", "express"]
+  - ["Postgresql", "postgresql"]
+---
--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -7,5 +7,11 @@ export const stackImage: { [key: string]: string } = {
     "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
   vue: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
   "vue-router": "https://router.vuejs.org/logo.png",
-  arch: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Archlinux-icon-crystal-64.svg/1200px-Archlinux-icon-crystal-64.svg.png",
+  arch: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Archlinux-logo-standard-version.png/250px-Archlinux-logo-standard-version.png",
+  react:
+    "https://glints.com/id/lowongan/wp-content/uploads/2020/10/logo-reactjs.jpg",
+  reactquery:
+    "https://tkdodo.eu/blog/static/e34c9bdb709f49c90b76af647160ca18/73f08/react-query.png",
+  express: "/assets/express.jpeg",
+  postgresql: "/assets/psql.png",
 };
